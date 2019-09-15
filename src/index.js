006d66744a84d44bb00fac6f4833ff89bc6fb9c6
sartoriusrex <dennis.q.mai@gmail.com>
2019-09-14 22:45:16 -1000

    add project section and finish about section

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './styles/style.scss';
+import Biopic from './images/bio.jpeg';
+
+function component() {
+	const pic = document.querySelector("#biopic");
+
+	pic.src = Biopic;
+}
+
+component();
+
+const intersectionObserver = new IntersectionObserver( function( entries ) {
+	if ( entries[0].intersectionRatio <= 0 ) {
+		return;
+	}
+
+	
+});
+
+intersectionObserver.observe(document.querySelector("#portfolio"));
