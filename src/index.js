28338ac1e6e7e51c92140a358a75942c5a57cbe6
sartoriusrex <dennis.q.mai@gmail.com>
2019-09-16 11:38:33 -1000

    create footer and add in links and icons, and set src via javascript; begin creating contact form

--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ import usdtImageSm from './images/projects/usdt-sm.png';
 import usdtImageSmPlaceholder from './images/placeholders/usdt-sm-ph.png';
 import ravenImageSm from './images/projects/raven-sm.png';
 import ravenImageSmPlaceholder from './images/placeholders/raven-sm-ph.png';
+import pdf from './images/icons/pdf.svg';
+import linkedin from './images/icons/linkedin.svg';
+import github from './images/icons/github.svg';
+import stackoverflow from './images/icons/stackoverflow.svg';
+import skype from './images/icons/skype.svg';
+import resume from './images/mai_resume.pdf';
 
 
 let biopic = document.querySelector("#biopic");
@@ -23,6 +29,37 @@ function mountImages() {
 
 mountImages();
 
+
+// Mount Links
+let resumeLink = document.querySelector("#resume");
+let resumeLinkTwo = document.querySelector("#resume-link");
+
+function mountLinks() {
+	resumeLink.href = resume;
+	resumeLinkTwo.href = resume;
+}
+
+mountLinks();
+
+
+// Mount Icons
+let pdfIcon = document.querySelector("#pdf-icon");
+let linkedinIcon = document.querySelector("#linkedin-icon");
+let githubIcon = document.querySelector("#github-icon");
+let stackoverflowIcon = document.querySelector("#stackoverflow-icon");
+let skypeIcon = document.querySelector("#skype-icon");
+
+function mountIcons(){
+	pdfIcon.src = pdf;
+	linkedinIcon.src = linkedin;
+	githubIcon.src = github;
+	stackoverflowIcon.src = stackoverflow;
+	skypeIcon.src = skype;
+}
+
+mountIcons();
+
+// Intersection Observer to lazyload images
 const intersectionObserver = new IntersectionObserver( function( entries ) {
 	if ( entries[0].intersectionRatio <= 0 ) {
 		return;
