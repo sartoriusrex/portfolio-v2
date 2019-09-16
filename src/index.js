c052c78553fc829bbd88be99847086b7aa5cb2ca
sartoriusrex <dennis.q.mai@gmail.com>
2019-09-15 21:33:46 -1000

    import images and src them in portfolio section; style the portfolio cards

--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
 import './styles/style.scss';
-import Biopic from './images/bio.jpeg';
+import bioImage from './images/bio.jpeg';
+import bioImagePlaceholder from './images/placeholders/bio-ph.jpeg';
+import jjdImageSm from './images/projects/jjd-sm.png';
+import jjdImageSmPlaceholder from './images/placeholders/jjd-sm-ph.png';
+import usdtImageSm from './images/projects/usdt-sm.png';
+import usdtImageSmPlaceholder from './images/placeholders/usdt-sm-ph.png';
+import ravenImageSm from './images/projects/raven-sm.png';
+import ravenImageSmPlaceholder from './images/placeholders/raven-sm-ph.png';
 
-function component() {
-	const pic = document.querySelector("#biopic");
 
-	pic.src = Biopic;
+let biopic = document.querySelector("#biopic");
+let jjdpic = document.querySelector("#jjd");
+let usdtpic = document.querySelector("#usdt");
+let ravenpic = document.querySelector("#raven");
+
+function mountImages() {
+	biopic.src = bioImagePlaceholder;
+	jjdpic.src = jjdImageSmPlaceholder;
+	usdtpic.src = usdtImageSmPlaceholder;
+	ravenpic.src = ravenImageSmPlaceholder;
 }
 
-component();
+mountImages();
 
 const intersectionObserver = new IntersectionObserver( function( entries ) {
 	if ( entries[0].intersectionRatio <= 0 ) {
 		return;
 	}
 
-	
+
 });
 
 intersectionObserver.observe(document.querySelector("#portfolio"));
