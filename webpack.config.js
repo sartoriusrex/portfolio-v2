fb127b636dec29c7a6e9cce4dbaa3f34b7dd530a
sartoriusrex <dennis.q.mai@gmail.com>
2019-09-18 12:31:46 -1000

    add critters to inline critical css

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// const HtmlCriticalWebpackPlugin = require("html-critical-webpack-plugin");
+const Critters = require('critters-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
@@ -30,19 +30,9 @@ module.exports = {
 		new MiniCssExtractPlugin({
 			filename: "style.css",
 		}),
-		// new HtmlCriticalWebpackPlugin({
-    //   base: path.resolve(__dirname, 'dist'),
-    //   src: 'index.html',
-    //   dest: 'index.html',
-    //   inline: true,
-    //   minify: true,
-    //   extract: true,
-    //   width: 375,
-    //   height: 565,
-    //   penthouse: {
-    //     blockJSRequests: false,
-    //   }
-    // })
+		new Critters({
+			fonts: true,
+		})
 	],
 	module: {
 		rules: [
