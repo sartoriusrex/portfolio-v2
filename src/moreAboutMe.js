83d1140e32ec3a6cb17d3c06ea231bd6dbab8b00
sartoriusrex <dennis.q.mai@gmail.com>
2019-10-23 10:50:11 -1000

    add padding to aboutme; add focusout event on moreaboutme

--- src/moreAboutMe.js
+++ src/moreAboutMe.js
@@ -3,8 +3,12 @@ let moreAboutMeDiv = document.getElementById('more-about-me');
 
 moreAboutMeButton.addEventListener("click", function(){
 	moreAboutMeDiv.style.left = "0";
-})
+});
 
 moreAboutMeDiv.addEventListener("click", function(){
 	moreAboutMeDiv.style.left = "-100%";
-})
+});
+
+moreAboutMeDiv.addEventListener("focusout", function(){
+	moreAboutMeDiv.style.left = "-100%";
+});
