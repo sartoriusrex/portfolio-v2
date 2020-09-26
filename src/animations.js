fc162db56d47c0738baf3cb8eb6427ee72ef6166
sartoriusrex <dmai.developer@gmail.com>
2020-09-25 21:01:20 -1000

    begin to use howler! yay! get to work on animating some stuff with gsap! Cool! update some little things and make fill an svg property of the paths instead of defined in style, so gsap can animate it! we will see what else we will need to do

--- src/animations.js
+++ src/animations.js
@@ -1,6 +1,12 @@
 import { gsap } from 'gsap';
 
-gsap.to('.landing svg', {
-    duration: 5,
-    rotationX: 360,
+gsap.from('path', {
+    duration: 1.5,
+    // attr: {
+    //     transform: 'matrix(0,1,0,1,0,0)'
+    // },
+    attr: {
+        fill: 'black'
+    },
+    ease: "rough ({template: none.out, strength: 10, points: 50,taper: 'out', randomize: false, clamp: false })"
 })
