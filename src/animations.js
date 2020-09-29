52c4da35af7725b5ca372919affe899652116ab7
sartoriusrex <dmai.developer@gmail.com>
2020-09-28 21:25:38 -1000

    play with animation - attempt to only run it once, but I may have a problem with imports calling the code; update navigation and style to remove red outline

--- src/animations.js
+++ src/animations.js
@@ -3,8 +3,8 @@ import { gsap } from 'gsap';
 gsap.from('#logo path', {
     duration: 1.5,
     attr: {
-        fill: 'black'
+        fill: '#222222'
     },
-    ease: "rough ({template: none.out, strength: 10, points: 50,taper: 'out', randomize: false, clamp: false })"
+    // ease: "rough ({template: none.out, strength: 10, points: 50,taper: 'out', randomize: false, clamp: false })"
+    ease: "back.inOut(1.7)"
 });
-
