caf466ce0d50f8df670faaa2b00685e9a4733d6d
sartoriusrex <dmai.developer@gmail.com>
2020-10-03 21:46:27 -1000

    move hero animation to gsap and start experimenting with it; remove animation from hero.scss; restyle links in what you will find here section to make the more prominent

--- src/scripts/animations.js
+++ src/scripts/animations.js
@@ -8,3 +8,16 @@ gsap.from('#logo path', {
     // ease: "rough ({template: none.out, strength: 10, points: 50,taper: 'out', randomize: false, clamp: false })"
     ease: "back.inOut(1.7)"
 });
+
+// const heroTL = gsap.timeline({
+// repeat: infinite,
+// })
+
+gsap.to('#hero-cube', {
+    duration: 7,
+    rotationX: 360,
+    rotationY: 360,
+    ease: 'none',
+    repeat: 2,
+})
+
