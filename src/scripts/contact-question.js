8f06b193d13ed33442f9d1101a048117ab6065c3
sartoriusrex <dmai.developer@gmail.com>
2020-10-12 21:38:07 -1000

    edit the header svgs to be a bit finer; update contact styling and a bit of contact js; begin illustration for egg

--- src/scripts/contact-question.js
+++ src/scripts/contact-question.js
@@ -1,1 +1,7 @@
 const emailLink = document.getElementById('email-link');
+const submitBtn = document.getElementById('submit-answer');
+
+submitBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+})
