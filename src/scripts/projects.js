20af02064ad0238ee23053d3a7ec235f2f1bd0ed
sartoriusrex <dmai.developer@gmail.com>
2020-11-28 10:44:02 +0100

    add styles on focus, active, and hover and style projects and update keys for projects.js

--- src/scripts/projects.js
+++ src/scripts/projects.js
@@ -3,8 +3,8 @@ const projects = [
         name: 'Polyglotist',
         description: 'An application for intermediate language learners to improve reading fluency and vocabulary. Users can read foreign language newspapers, as well as look up words and phrases  and save them. Looking up and saving can be done in a total of 3 clicks, a drastic speed from  leaving the app or window to look up and document a word. This helps learners improve fluency with increasing focus on the text at hand instead of abruptly switching contexts.',
         links: {
-            github: 'https://github.com/sartoriusrex/polyglotist',
-            live: ''
+            Github: 'https://github.com/sartoriusrex/polyglotist',
+            Live: ''
         },
         images: [
             'imgs/polyglotist-1.png',
@@ -17,9 +17,9 @@ const projects = [
         name: 'Jiu Jitsu Distilled',
         description: 'A web application for users to document and share Brazilian >%Jiu Jitsu techniques.Users can make noteson techniques and link to videos, connect techniques insequences, like them, and share them in -app with other usersor by e - mail.There are hundreds of grappling techniques and manyvariations between them.Flowing from one toanother is the essence of Jiu Jitsu, but understanding howand when they can move from one to another is a largeundertaking.This application helps document the relationshipsthey have with each other.',
         links: {
-            'github-front': 'https://github.com/sartoriusrex/jjd-client',
-            'github-back': 'https://github.com/sartoriusrex/jjd-server-public',
-            live: 'jjd-client-v1.herokuapp.com/'
+            'Github Front-end': 'https://github.com/sartoriusrex/jjd-client',
+            'Github Back-end': 'https://github.com/sartoriusrex/jjd-server-public',
+            Live: 'https://jjd-client-v1.herokuapp.com/'
         },
         images: [
             'imgs/jjd-sm.png',
@@ -30,8 +30,8 @@ const projects = [
         name: 'US Department of Truth',
         description: "A simple server-side site satirizing the the US's current relationship with misinformation and disinformation.It was designed to both educate andpoke fun at the news /media and the government.Read everythingwith 2 grains of salt, some lime, and a bit of tequila.",
         links: {
-            github: 'https://github.com/sartoriusrex/USDT',
-            live: 'https://usdt-satire.herokuapp.com/'
+            Github: 'https://github.com/sartoriusrex/USDT',
+            Live: 'https://usdt-satire.herokuapp.com/'
         },
         images: [
             'imgs/usdt-sm.png',
@@ -42,9 +42,9 @@ const projects = [
         name: 'This Portfolio!',
         description: "This is my website that you're looking at now! For more information, you can read the post provided in he link below.",
         links: {
-            github: 'https://github.com/sartoriusrex/portfolio-v2',
+            Github: 'https://github.com/sartoriusrex/portfolio-v2',
             'about this blog': '/how-i-made-this-blog.html',
-            live: 'https://www.dennismai.dev/'
+            Live: 'https://www.dennismai.dev/'
         },
         images: [
             'imgs/fam.png'
