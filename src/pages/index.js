240d4abebed252c628cd994bb9696189e050b499
sartoriusrex <dmai.developer@gmail.com>
2020-12-05 16:04:33 +0100

    remove images import - unnecessary?

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import '../styles/index.scss';
-import '../scripts/images';
+// import '../scripts/images';
 import { animateLogo, fallingParagraphs } from '../scripts/animations';
 import '../scripts/hero';
 import '../scripts/burger-menu';
