59746b8c09e26f2450b1655325c66effa2b8320e
sartoriusrex <dmai.developer@gmail.com>
2020-12-05 21:27:50 +0100

    add aria labels to menu and change aria expanded

--- src/scripts/burger-menu.js
+++ src/scripts/burger-menu.js
@@ -8,10 +8,12 @@ function toggleBurger() {
 
     if (burgerValue === 'closed') {
         burgerButton.setAttribute('data-burger', 'open');
+        burgerButton.setAttribute('aria-expanded', true);
         burgerMenu.setAttribute('data-burger', 'open');
         disableScroll();
     } else {
         burgerButton.setAttribute('data-burger', 'closed');
+        burgerButton.setAttribute('aria-expanded', false);
         burgerMenu.setAttribute('data-burger', 'closed');
         enableScroll();
     }
