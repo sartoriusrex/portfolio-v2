b41d4e143dc9371782a3f9cbc992bc0062793f65
sartoriusrex <dmai.developer@gmail.com>
2020-12-13 11:48:31 +0100

    add skillsList to import into projecs; begin to add skills list in projects; update about to include section about programming and link to projects

---
+++ src/scripts/skillsList.js
@@ -0,0 +1,64 @@
+export const skills = {
+    languages: [
+        'HTML',
+        'CSS',
+        'Javascript',
+        'Typescript',
+        'Node',
+        'Python',
+        'ejs',
+    ],
+    styling: [
+        'CSS',
+        'SCSS',
+        'BEM',
+        'Styled-Components',
+        'CSS-in-JS',
+        'CSS-in-CSS',
+        'CSS Modules',
+        'Tailwind',
+        'Bootstrap',
+        'SemanticUI',
+    ],
+    frameworks: [
+        'React + Redux',
+        'Apollo',
+        'Express',
+        'Electron',
+    ],
+    databases: [
+        'PostgresQL',
+        'mySQL',
+        'GraphQL',
+        'MongoDB',
+        'NeDB',
+        'Redis'
+    ],
+    devops: [
+        'Docker',
+        'Webpack',
+        'Babel',
+        'Gulp',
+        'Jest',
+        'Enzyme',
+        'React-Testing-Library',
+        'TravisCI',
+        'Heroku',
+        'GitlabCI',
+        'Netlify'
+    ],
+    collaboration: [
+        'Git',
+        'Github, Bitbucket, Gitlab',
+        'Jira',
+        'Salesforce',
+        'Google & Microsoft Productivity Suites'
+    ],
+    design: [
+        'Figma',
+        'Zeplin',
+        'Gimp',
+        'Inkscape',
+        'Adobe XD'
+    ]
+}
