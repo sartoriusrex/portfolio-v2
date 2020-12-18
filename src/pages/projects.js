53a788d4543a84c32d7854fae23b64c3abbff173
sartoriusrex <dmai.developer@gmail.com>
2020-12-18 20:36:36 +0100

    add fxs to skill carousel to export to pages/projects and keep on stylin

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,6 +1,13 @@
 import '../styles/projects.scss';
 import '../scripts/burger-menu';
 import { appearingProjects } from '../scripts/animations';
-import '../scripts/skill-carousel';
+import { toggleVisible, clickLeft, clickRight } from '../scripts/skill-carousel';
 
 appearingProjects();
+
+// show first element
+toggleVisible();
+
+//click listeners for buttons
+clickLeft();
+clickRight();
